ce828c7ea9ff8eeb16a38c173a6ec6d1bc03e715
sklukin <sklukin@yandex.ru>
2017-01-22 17:37:47 +0500

    First commit

---
+++ myapp.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/env perl
+use Mojolicious::Lite;
+
+# Documentation browser under "/perldoc"
+plugin 'PODRenderer';
+
+plugin 'Config' => { 'main.conf' };
+plugin 'Config' => { 'main.conf' };
+
+plugin 'mail' => {
+  from => 'mail@sklukin.ru',
+  type => 'text/html; charset="utf-8"'
+};
+
+get '/' => sub {
+  my $c = shift;
+  $c->render(template => 'index');
+};
+
+app->start;
+
+__DATA__
+
+@@ index.html.ep
+% layout 'default';
+% title 'Welcome';
+<h1>Welcome to the Mojolicious real-time web framework!</h1>
+To learn more, you can browse through the documentation
+<%= link_to 'here' => '/perldoc' %>.
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%= title %></title></head>
+  <body><%= content %></body>
+</html>
