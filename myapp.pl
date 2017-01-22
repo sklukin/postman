36b2d5c4b21681e0d02f2d5e6fd4bc9d59ab6f74
sklukin <sklukin@yandex.ru>
2017-01-22 21:20:11 +0500

    Рабочая версия

--- myapp.pl
+++ myapp.pl
@@ -8,7 +8,7 @@ plugin 'PODRenderer';
 plugin 'Config' => { file => 'main.conf' };
 
 plugin 'mail' => {
-  from => 'mail@sklukin.ru',
+  from => 'info@sklukin.ru',
   type => 'text/html; charset="utf-8"'
 };
 
@@ -19,7 +19,7 @@ get '/' => sub {
 
 post '/send' => sub {
   my $c = shift;
-  my $message = '';
+  my $error  = '';
   my @params = (qw/to from subject data/);
   my %params;
 
@@ -27,57 +27,16 @@ post '/send' => sub {
     return $c->reply->not_found unless $params{$p} = $c->param($p);
   }
 
-  $c->mail(%params);
-
-  # $c->reply->not_found unless my $msg = $c->param('msg');
-
-  # p $msg;
-
-  # $c->mail(
-  #   to      => 'sklukin@yandex.ru',
-  #   subject => 'Hi',
-  #   data    => 'use Perl or die',
-  # );
-
-  # p $msg;
-
-  # # $c->mail(mail => $msg);
-  # # my @args = ('/usr/bin/sendmail', $msg;
-  # # system (@args) or $message = 'Cant send mail'
-
-  # my @cmd = ( 'sendmail', '-t', '-oi', '-oem' );
-
-  # # # SetSender default is true
-  # # $p{SetSender} = 1 unless defined $p{SetSender};
-
-  # # ### See if we are forcibly setting the sender:
-  # # $p{SetSender} ||= defined( $p{FromSender} );
-
-  # # ### Add the -f argument, unless we're explicitly told NOT to:
-  # # if ( $p{SetSender} ) {
-  # #     my $from = $p{FromSender} || ( $self->get('From') )[0];
-  # #     if ($from) {
-  # #         my ($from_addr) = extract_full_addrs($from);
-  # #         push @cmd, "-f$from_addr" if $from_addr;
-  # #     }
-  # # }
-  # push @cmd, $msg;
-
-  # ### Open the command in a taint-safe fashion:
-  # my $pid = open SENDMAIL, "|-";
-  # defined($pid) or die "open of pipe failed: $!\n";
-  # if ( !$pid ) {    ### child
-  #     exec(@cmd) or die "can't exec sendmail: $!\n";
-  #     ### NOTREACHED
-  # } else {          ### parent
-  #     # $self->print( \*SENDMAIL );
-  #     close SENDMAIL || die "error closing sendmail: $! (exit $?)\n";
-  #     # $return = 1;
-  # }
+  eval {
+    $c->mail(%params);
+    1;
+  } or do {
+    $error = "Sent error - $@"
+  };
 
   $c->render(json => {
-    success => 1,
-    message => $message,
+    success => $error ? 0 : 1,
+    message => $error,
   });
 };
 
