ebdf0d09b610e31193821feeca19e3aa4e75a1f4
sklukin <sklukin@yandex.ru>
2017-01-22 19:44:24 +0500

    update

--- myapp.pl
+++ myapp.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use DDP;
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
-plugin 'Config' => { 'main.conf' };
-plugin 'Config' => { 'main.conf' };
+plugin 'Config' => { file => 'main.conf' };
 
 plugin 'mail' => {
   from => 'mail@sklukin.ru',
@@ -15,6 +15,62 @@ plugin 'mail' => {
 get '/' => sub {
   my $c = shift;
   $c->render(template => 'index');
+};
+
+post '/send' => sub {
+  my $c = shift;
+  my $message = '';
+
+  $c->reply->not_found unless my $msg = $c->param('msg');
+
+  p $msg;
+
+  $c->mail(
+    to      => 'sklukin@yandex.ru',
+    subject => 'Hi',
+    data    => 'use Perl or die',
+  );
+
+  p $msg;
+
+  # # $c->mail(mail => $msg);
+  # # my @args = ('/usr/bin/sendmail', $msg;
+  # # system (@args) or $message = 'Cant send mail'
+
+  # my @cmd = ( 'sendmail', '-t', '-oi', '-oem' );
+
+  # # # SetSender default is true
+  # # $p{SetSender} = 1 unless defined $p{SetSender};
+
+  # # ### See if we are forcibly setting the sender:
+  # # $p{SetSender} ||= defined( $p{FromSender} );
+
+  # # ### Add the -f argument, unless we're explicitly told NOT to:
+  # # if ( $p{SetSender} ) {
+  # #     my $from = $p{FromSender} || ( $self->get('From') )[0];
+  # #     if ($from) {
+  # #         my ($from_addr) = extract_full_addrs($from);
+  # #         push @cmd, "-f$from_addr" if $from_addr;
+  # #     }
+  # # }
+  # push @cmd, $msg;
+
+  # ### Open the command in a taint-safe fashion:
+  # my $pid = open SENDMAIL, "|-";
+  # defined($pid) or die "open of pipe failed: $!\n";
+  # if ( !$pid ) {    ### child
+  #     exec(@cmd) or die "can't exec sendmail: $!\n";
+  #     ### NOTREACHED
+  # } else {          ### parent
+  #     # $self->print( \*SENDMAIL );
+  #     close SENDMAIL || die "error closing sendmail: $! (exit $?)\n";
+  #     # $return = 1;
+  # }
+
+  $c->render(json => {
+    success => 1,
+    message => $message,
+  });
 };
 
 app->start;
